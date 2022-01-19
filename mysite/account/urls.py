58b955cd6c49b08cae507021fd61800fb49e3d91
yy340 <yy340@duke.edu>
2022-01-19 14:43:24 -0500

    start

--- mysite/account/urls.py
+++ mysite/account/urls.py
@@ -3,9 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('login/',views.djlogin,name='login'),
-=======
-    path('',views.index,name='index'), #<a href="{% url 'index' %}">Home</a>. Link to this page in HTML
->>>>>>> 27d2912d9b8407105e1bef2011080bc563f0a686
+    path('index/',views.index),
+    path('accounts/login/',views.login),# 'accounts/login' is the default page of Django login page
 ]
--- mysite/account/views.py
+++ mysite/account/views.py
@@ -1,49 +1,21 @@
-
-<<<<<<< HEAD
 from django.shortcuts import render,redirect
 from django.contrib import auth
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 
-def djlogin(request):
 
-    if request.method == 'GET':
-        return render(request, 'login.html')
 
-    if request.method == 'POST':
-
-        name = request.POST.get('name')
-        password = request.POST.get('password')
-        # 验证用户名和密码，验证通过的话，返回user对象
-        user = auth.authenticate(username=name, password=password)
-        if user:
-            # 验证成功 登陆
-            auth.login(request, user)
-            return HttpResponseRedirect('/stuapp/index/')
-        else:
-            return render(request, 'day6_regist.html')
-
-def djregist(request):
-
-    if request.method == 'GET':
-        return render(request, 'day6_regist.html')
-
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        password = request.POST.get('password')
-        # 此处的User 是 django 自带的model
-        User.objects.create_user(username=name, password=password)
-        return HttpResponseRedirect('/uauth/dj_login/')
-
-def djlogout(request):
-
-    if request.method == 'GET':
-
-        auth.logout(request)
-        return HttpResponseRedirect('/uauth/dj_login/')
-=======
-
-# Create your views here.
 def index(request):
     return render(request, 'index.html')
->>>>>>> 27d2912d9b8407105e1bef2011080bc563f0a686
+
+def login(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('/index/')
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username=username, password=password)
+    if user is not None and user.is_active:
+        auth.login(request, user)
+        return HttpResponseRedirect('/index/')
+    else:
+        return render(request, 'login.html', locals())
